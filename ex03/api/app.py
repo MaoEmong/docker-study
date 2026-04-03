a18c05b13d79860e31f0c01fd775b58a61e05919
emong <a34451357@gmail.com>
2026-04-03 09:53:06 +0900

    ex03까지 진행

--- ex03/api/app.py
+++ ex03/api/app.py
@@ -3,6 +3,7 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     html = """
@@ -14,12 +15,14 @@ def index():
     </html>
     """
     return Response(html, mimetype="text/html")
-  
+
+
 # 이미지 요청
 @app.route("/image.png")
 def get_image():
     image_path = os.path.join(os.path.dirname(__file__), "image.png")
     return send_file(image_path, mimetype="image/png")
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4000, debug=True)
